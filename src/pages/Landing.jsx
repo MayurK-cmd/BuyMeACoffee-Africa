e70481c70175eacc8e2f3c10dd1cb0114c3ff9e3
MayurK-cmd <mayurgk2006@gmail.com>
2025-10-23 19:43:50 +0530

    fix: force update pnpm-lock.yaml to sync with package.json

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,6 @@
 function Landing(){
-    return(
+    
+  return(
         <div className="flex flex-col justify-center items-center h-screen bg-blue-200">
         <h1 className="text-3xl font-bold">Welcome to BuyMeACoffee-Africa</h1>
         <p className="mt-4">
